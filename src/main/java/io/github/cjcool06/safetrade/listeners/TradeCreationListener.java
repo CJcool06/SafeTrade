9ca204fbe006f2e07c3396c573fc9571b3d7df47
CJcool06 <30566151+CJcool06@users.noreply.github.com>
2019-02-11 21:01:52 +1030

    Added listeners

---
+++ src/main/java/io/github/cjcool06/safetrade/listeners/TradeCreationListener.java
@@ -0,0 +1,25 @@
+package io.github.cjcool06.safetrade.listeners;
+
+import io.github.cjcool06.safetrade.api.events.trade.TradeCreationEvent;
+import io.github.cjcool06.safetrade.obj.Trade;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.action.TextActions;
+import org.spongepowered.api.text.format.TextColors;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TradeCreationListener {
+
+    @SubscribeEvent
+    public void onHandshake(TradeCreationEvent event) {
+        Trade trade = event.trade;
+        trade.sendMessage(Text.of(TextColors.GREEN, "Trade channel initialised."));
+        try {
+            trade.sendMessage(Text.builder().append(Text.of(TextColors.GOLD, "If you're unsure about this chat or how to conduct a SafeTrade, click here.")).onClick(TextActions.openUrl(new URL("https://github.com/CJcool06/SafeTrade/wiki"))).build());
+        } catch (MalformedURLException mue) {
+            trade.sendMessage(Text.of(TextColors.GOLD, "If you're unsure about this chat or how to conduct a SafeTrade, type /safetrade wiki"));
+        }
+    }
+}
---
+++ src/main/java/io/github/cjcool06/safetrade/listeners/ViewerConnectionListener.java
@@ -0,0 +1,19 @@
+package io.github.cjcool06.safetrade.listeners;
+
+import io.github.cjcool06.safetrade.api.events.trade.ViewerEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+
+public class ViewerConnectionListener {
+
+    @SubscribeEvent
+    public void onViewer(ViewerEvent event) {
+        if (event instanceof ViewerEvent.Add) {
+            event.trade.sendMessage(Text.of(TextColors.GOLD, event.viewer.getName(), TextColors.GREEN, " is viewing the trade."));
+        }
+        else if (event instanceof ViewerEvent.Remove) {
+            event.trade.sendMessage(Text.of(TextColors.GOLD, event.viewer.getName(), TextColors.RED, " is no longer viewing the trade."));
+        }
+    }
+}
