a8c156cfdb561fdffdfdab51299dbe531cf65348
CJcool06 <30566151+CJcool06@users.noreply.github.com>
2019-03-17 17:46:22 +1030

    + Config broadcast check + Cleanup

--- src/main/java/io/github/cjcool06/safetrade/listeners/TradeExecutedListener.java
+++ src/main/java/io/github/cjcool06/safetrade/listeners/TradeExecutedListener.java
@@ -1,14 +1,20 @@
 package io.github.cjcool06.safetrade.listeners;
 
+import io.github.cjcool06.safetrade.SafeTrade;
 import io.github.cjcool06.safetrade.api.events.trade.TradeEvent;
+import io.github.cjcool06.safetrade.config.Config;
 import io.github.cjcool06.safetrade.utils.Utils;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class TradeExecutedListener {
 
     @SubscribeEvent
-    public void onExecuted(TradeEvent.Executed.SuccessfulTrade event) {
-        event.trade.getSides()[0].sendMessage(Utils.getSuccessMessage(event.trade));
-        event.trade.getSides()[1].sendMessage(Utils.getSuccessMessage(event.trade));
+    public void onExecuted(TradeEvent.Executed.Success event) {
+        if (Config.broadcastTradeOverviews) {
+            SafeTrade.broadcast(Utils.getBroadcastOverview(event.trade));
+        }
+        else {
+            event.trade.sendMessage(Utils.getSuccessMessage(event.trade));
+        }
     }
 }
