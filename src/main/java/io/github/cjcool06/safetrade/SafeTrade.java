45a5e71bfbcf0f3d86ac1ad9bb01de105e574782
CJcool06 <cjcool06@yahoo.com.au>
2021-06-01 09:16:29 +0930

    Version control

--- src/main/java/io/github/cjcool06/safetrade/SafeTrade.java
+++ src/main/java/io/github/cjcool06/safetrade/SafeTrade.java
@@ -43,7 +43,7 @@ import java.util.concurrent.TimeUnit;
 public class SafeTrade {
     public static final String ID = "safetrade";
     public static final String NAME = "SafeTrade";
-    public static final String VERSION = "3.2.1";
+    public static final String VERSION = "3.2.2";
     public static final String DESCRIPTION = "Trade Pokemon, Items, and Money safely";
     public static final String AUTHORS = "CJcool06";
 
